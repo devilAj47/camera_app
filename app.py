da16f54e811fcae4660794f7d34d557176bdae0f
devilAj47 <52520467+devilAj47@users.noreply.github.com>
2021-04-02 11:32:58 +0530

    Socket Connection

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import cv2
 #Initialize the Flask app
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
-'''
-for ip camera use - rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' 
-for local webcam use cv2.VideoCapture(0)
-'''
+
 
 def gen_frames():  
     while True:
